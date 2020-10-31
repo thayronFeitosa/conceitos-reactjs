a6f1613e50373299ea70db6be00bd728b0babe20
thayron <thayronf3@gmail.com>
2020-10-31 13:48:52 -0300

    Foi realizado todos os testes do desafio proprosto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setPepositoriess] = useState([]);
+  useEffect(() => {
+    api.get('repositories').then(repositories => {
+      setPepositoriess(repositories.data)
+    })
+  }, [])
+
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"],
+    });
+
+    setPepositoriess([...repositories, response.data])
+
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setPepositoriess(repositories.filter(
+      repository => repository.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => <li key={repository.id}>{repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover</button></li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
